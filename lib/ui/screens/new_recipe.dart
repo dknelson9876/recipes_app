a84d74ba91da26caa147aa7a4c24f7861efc8da7
dknelson9876 <dknelson9876@gmail.com>
2022-03-05 12:14:02 -0700

    Remove the ListView wrapper because that just made the BottomSheet take up the whole screen

--- lib/ui/screens/new_recipe.dart
+++ lib/ui/screens/new_recipe.dart
@@ -13,87 +13,85 @@ Future<void> createRecipe(BuildContext context, RecipeType recipeType) async {
     isScrollControlled: true,
     context: context,
     builder: (BuildContext context) {
-      return ListView(children: [
-        Padding(
-          padding: EdgeInsets.only(
-            top: 20,
-            left: 20,
-            right: 20,
-            bottom: MediaQuery.of(context).viewInsets.bottom + 20,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextField(
-                controller: _nameController,
-                decoration: const InputDecoration(labelText: 'Name'),
-              ),
-              TextField(
-                controller: _timeController,
-                decoration: const InputDecoration(labelText: 'Time'),
-              ),
-              TextField(
-                controller: _ingredientsController,
-                decoration: const InputDecoration(
-                  labelText: 'Ingredients',
-                  helperText: 'Separate list using semicolons \';\'',
-                ),
-              ),
-              TextField(
-                controller: _preparationController,
-                decoration: const InputDecoration(
-                  labelText: 'Preparation Steps',
-                  helperText: 'Separate list using semicolons \';\'',
-                ),
-              ),
-              TextField(
-                controller: _imageLinkController,
-                decoration: const InputDecoration(labelText: 'Link to image'),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  const Spacer(),
-                  ElevatedButton(
-                    child: const Text('Submit'),
-                    onPressed: () async {
-                      // print('submit');
-
-                      final String? name = _nameController.text;
-                      final int? duration = int.tryParse(_timeController.text);
-                      final List<String>? ingredients =
-                          _ingredientsController.text.split(';');
-                      final List<String>? preparation =
-                          _preparationController.text.split(';');
-                      final String? imageLink = _imageLinkController.text;
-
-                      Object recipe = {
-                        'name': name,
-                        'duration': duration ?? 0,
-                        'ingredients': ingredients,
-                        'preparation': preparation,
-                        'image': imageLink,
-                        'type': recipeType.index,
-                      };
-
-                      StateWidget.of(context).uploadRecipe(context, recipe);
-
-                      _nameController.clear();
-                      _timeController.clear();
-                      _ingredientsController.clear();
-                      _preparationController.clear();
-                      _imageLinkController.clear();
-
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
-              ),
-            ],
-          ),
+      return Padding(
+        padding: EdgeInsets.only(
+          top: 20,
+          left: 20,
+          right: 20,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 20,
         ),
-      ]);
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: _timeController,
+              decoration: const InputDecoration(labelText: 'Time'),
+            ),
+            TextField(
+              controller: _ingredientsController,
+              decoration: const InputDecoration(
+                labelText: 'Ingredients',
+                helperText: 'Separate list using semicolons \';\'',
+              ),
+            ),
+            TextField(
+              controller: _preparationController,
+              decoration: const InputDecoration(
+                labelText: 'Preparation Steps',
+                helperText: 'Separate list using semicolons \';\'',
+              ),
+            ),
+            TextField(
+              controller: _imageLinkController,
+              decoration: const InputDecoration(labelText: 'Link to image'),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                const Spacer(),
+                ElevatedButton(
+                  child: const Text('Submit'),
+                  onPressed: () async {
+                    // print('submit');
+
+                    final String? name = _nameController.text;
+                    final int? duration = int.tryParse(_timeController.text);
+                    final List<String>? ingredients =
+                        _ingredientsController.text.split(';');
+                    final List<String>? preparation =
+                        _preparationController.text.split(';');
+                    final String? imageLink = _imageLinkController.text;
+
+                    Object recipe = {
+                      'name': name,
+                      'duration': duration ?? 0,
+                      'ingredients': ingredients,
+                      'preparation': preparation,
+                      'image': imageLink,
+                      'type': recipeType.index,
+                    };
+
+                    StateWidget.of(context).uploadRecipe(context, recipe);
+
+                    _nameController.clear();
+                    _timeController.clear();
+                    _ingredientsController.clear();
+                    _preparationController.clear();
+                    _imageLinkController.clear();
+
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
     },
   );
 }
